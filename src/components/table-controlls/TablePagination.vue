ff98d9164c1208c6861045de303fdcf8e649c3de
Lutfi <lutfihakim288@gmail.com>
2025-05-30 15:10:13 +0700

    Fix pagination behavior on filter and sorter change

--- src/components/table-controlls/TablePagination.vue
+++ src/components/table-controlls/TablePagination.vue
@@ -1,53 +1,82 @@
 <script setup lang="ts">
-  import type { Meta } from '@/utils/dtos/Meta';
-  import type { SelectItem } from '@nuxt/ui'
-  import { useRouteQuery } from '@vueuse/router'
-  import { computed, onMounted, ref, watch } from 'vue'
-  import { useRoute } from 'vue-router';
+import type { Meta } from '@/utils/dtos/Meta';
+import type { SelectItem } from '@nuxt/ui'
+import { useRouteQuery } from '@vueuse/router'
+import { computed, onMounted, ref, watch } from 'vue'
+import { useRoute } from 'vue-router';
 
-  const { meta, disabled } = defineProps<{
-    meta?: Meta,
-    disabled?: boolean
-  }>()
+const { meta, disabled, recordName = 'rows' } = defineProps<{
+  meta?: Meta,
+  disabled?: boolean,
+  recordName?: string
+}>()
 
-  const route = useRoute();
+const route = useRoute();
 
-  const page = useRouteQuery('page', undefined, { transform: Number })
-  const pageSize = useRouteQuery('pageSize', undefined, { transform: Number })
+const page = useRouteQuery('page', undefined, { transform: Number })
+const pageSize = useRouteQuery('pageSize', undefined, { transform: Number })
 
-  const availableSize = ref<SelectItem[]>([
-    5,
-    10,
-    20,
-    25,
-    50,
-    100,
-  ])
-  const totalData = ref<number>()
+const availableSize = ref<SelectItem[]>([
+  5,
+  10,
+  20,
+  25,
+  50,
+  100,
+])
+const totalData = ref<number>()
 
-  const dataRange = computed<number[]>(() => {
-    if (!meta) return [0, 0]
-    const start = ((meta.page - 1) * meta.pageSize) + 1;
-    const end = meta.page * meta.pageSize > meta.totalData ? meta.totalData : meta.page * meta.pageSize;
-    return [start, end]
-  })
+const dataRange = computed<number[]>(() => {
+  if (!meta) return [0, 0]
+  const start = ((meta.page - 1) * meta.pageSize) + 1;
+  const end = meta.page * meta.pageSize > meta.totalData ? meta.totalData : meta.page * meta.pageSize;
+  return [start, end]
+})
 
-  onMounted(() => {
+onMounted(() => {
+  if (!page.value) {
     page.value = 1
+  }
+  if (!pageSize.value) {
     pageSize.value = 10
-  })
+  }
+})
 
 watch(() => route.query, (value, oldValue) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { page: pageValue, ...restValue } = value;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { page: pageOldValue, ...restOldValue } = oldValue;
+  const restValue = { ...value };
+  delete restValue.page;
+
+  const restOldValue = { ...oldValue };
+  delete restOldValue.page;
+
+  let changedQuery: string = '';
 
   const otherChanged = Object.keys(restValue).some(
-    key => restValue[key] !== restOldValue[key]
+    key => {
+      if (Array.isArray(restValue[key])) {
+        const oldArray = (restOldValue[key] || []) as string[];
+        const currentArray = (restValue[key] || []) as string[];
+
+        const isChange = JSON.stringify(oldArray.sort()) !== JSON.stringify(currentArray.sort());
+
+        if (isChange) {
+          changedQuery = key;
+        }
+
+        return isChange;
+      }
+
+      const isChange = restValue[key] !== restOldValue[key];
+
+      if (isChange) {
+        changedQuery = key;
+      }
+
+      return isChange
+    }
   )
 
-  if (otherChanged) {
+  if (otherChanged && changedQuery && !['asc', 'desc'].includes(changedQuery)) {
     page.value = 1
   }
 
@@ -70,7 +99,7 @@ watch(() => meta, () => {
         <span class="text-highlighted">{{ dataRange[1] }}</span>
           of
         <span class="text-highlighted">{{ meta?.totalData }}</span>
-          items
+          {{ recordName }}
       </span>
     </p>
     <section class="flex items-center gap-x-4">
