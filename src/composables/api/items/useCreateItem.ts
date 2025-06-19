c6b9809542d7216f835d0be68ac58330cbde4168
Lutfi Hakim <lutfihakim288@gmail.com>
2025-06-19 09:42:51 +0700

    Update mutation on success to create more immersive item creation

--- src/composables/api/items/useCreateItem.ts
+++ src/composables/api/items/useCreateItem.ts
@@ -30,12 +30,12 @@ export function useCreateItem(listQueryKey: Ref<EntryKey>) {
 
       let newItems = [...oldItems];
 
-      if (searchParams.get('page') !== '1') {
+      if (searchParams.get('page') !== '1' && !searchParams.has('sort')) {
         return;
       }
 
       if (searchParams.has('keyword') && !item.name.toLowerCase().includes(searchParams.get('keyword')!.toLowerCase())) {
-        return
+        return;
       }
 
       const newItem = new Item({
@@ -46,20 +46,31 @@ export function useCreateItem(listQueryKey: Ref<EntryKey>) {
 
       newItems = [newItem, ...newItems]
 
-      if (searchParams.has('sort') && searchParams.get('sortDirection')) {
+      if (searchParams.has('sort') && searchParams.has('sortDirection')) {
         const key = searchParams.get('sort') as keyof Item;
         const direction = searchParams.get('sortDirection') as SortDirectionEnum;
 
         newItems = sort(newItems, key, direction);
       }
 
-      newItems.pop()
-
+      const pageSize = Number(searchParams.get('pageSize') || 10)
+      if (newItems.length >= pageSize) {
+        newItems.pop()
+      }
 
       queryCache.setQueryData(listQueryKey.value, newItems)
       queryCache.cancelQueries({ key: listQueryKey.value, exact: true })
 
       return { oldItems, newItems, newItem }
+    },
+    onSuccess(data, _vars, { newItems, newItem }) {
+      if (data && newItems && newItem) {
+        const _newItems = [...newItems]
+        const index = _newItems.indexOf(newItem)
+        _newItems[index] = data
+
+        queryCache.setQueryData(listQueryKey.value, _newItems)
+      }
     },
     async onSettled() {
       await queryCache.invalidateQueries({ key: PRIVATE_QUERY_KEYS.items.root() })
